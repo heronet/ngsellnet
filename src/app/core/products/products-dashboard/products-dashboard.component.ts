f68584249639726f8e2ea2f1efa15012d458bc6e
heronet <sirat4757@gmail.com>
2021-06-04 00:10:38 +0600

    Added loading indicator

--- src/app/core/products/products-dashboard/products-dashboard.component.ts
+++ src/app/core/products/products-dashboard/products-dashboard.component.ts
@@ -8,14 +8,27 @@ import { ProductsService } from 'src/app/services/products.service';
   styleUrls: ['./products-dashboard.component.scss']
 })
 export class ProductsDashboardComponent implements OnInit {
+  // UI
+  isloading = false;
+  errorMessage: string = null;
+  // Logic
   products: Product[] = [];
   totalItems = 0;
   constructor(private productsService: ProductsService) { }
 
   ngOnInit(): void {
+    this.getProducts();
+  }
+  getProducts() {
+    this.isloading  = true;
     this.productsService.getProducts().subscribe((response) => {
       this.products = response.data;
       this.totalItems = response.size;
+      this.isloading = false;
+      this.errorMessage = null;
+    }, error => {
+      this.isloading = false;
+      this.errorMessage = "An error occured";
     });
   }
 
